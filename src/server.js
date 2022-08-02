512d8d17a7f73850764b687b03bed94983a6f8f1
Chameli <Chameli@example.com>
2022-08-02 11:55:51 +0530

    connecting to the data base

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-const express= require('express')
+const express= require('express');
 require('dotenv').config();
-const bodyParser=require('body-parser')
+const bodyParser=require('body-parser');
+const dbConnection=require('./db/connection');
 const server=express();
-const router =require("./route/")
+const router =require("./route/");
 
 const port=process.env.PORT| 3001;
 
@@ -10,6 +11,7 @@ server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({extended:true}))
 server.use("/api",router);
 
+dbConnection();
 server.listen(port,function(){
     //console.log('hi',process.env)
     console.log('server started at port:',port);
